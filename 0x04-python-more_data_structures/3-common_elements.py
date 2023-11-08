175d1b4260e26712571666d764b077b82eb8cd3f
ZaneleNe <zanelenema97@gmail.com>
2023-11-08 04:09:34 -0800

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 
+"""
+    ------------
+    About Module
+    ------------
+    returns a set of common elements in two sets.
+
+    ---------
+    More Info
+    ---------
+    @set1: first set
+    @set2: second set
+"""
+
 
 def common_elements(set_1, set_2):
-    """
-    A function that returns a set of
-    common elements in two sets
-    """
-    return(set_1 & set_2)
+
+    """ Return the intersection of the two sets """
+
+    return (set_1 & set_2)
