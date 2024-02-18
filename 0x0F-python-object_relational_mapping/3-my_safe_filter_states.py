a9ab37eadc1870048d50f00e14056bda98243d2b
ZaneleNe <zanelenema97@gmail.com>
2024-02-18 08:40:18 -0800

    update

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-
+"""
+Filter states by user input safe from MySQL injections!
+It takes in an argument and displays all values in the states table
+"""
 
 import MySQLdb
 from sys import argv
 
-'''
-script that lists all states from the database
-'''
 if __name__ == "__main__":
-    cont = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
-    cursor = cont.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-        (argv[4],)
-        )
-    db = cursor.fetchall()
-    for i in db:
-        print(i)
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    query = "SELECT * FROM states\
+             WHERE states.name = %s\
+             ORDER BY states.id ASC"
+    cursor = db.cursor()
+    cursor.execute(query, (argv[4], ))
+    for state in cursor.fetchall():
+        print(state)
+    cursor.close()
+    db.close()
